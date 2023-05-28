b558aa6a9fafd66aa6b31da1f8090bf3ae783090
Nedimko123 <nedimlav@gmail.com>
2023-05-28 15:36:41 +0200

    added favicon.ico

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="flex flex-col mt-10 items-center">
       <p className="text-3xl font-extrabold mb-4">Get in Touch</p>
       <div className="flex flex-row gap-8 text-lg font-bold text-purple-600">
-        <a href="mailto:contact@nedo.ba">Email</a>
+        <a href="mailto:nedimlav@gmail.com">Email</a>
         <a href="https://www.linkedin.com/in/nedim-malicbegovic-43b56a149/">
           LinkedIn
         </a>
